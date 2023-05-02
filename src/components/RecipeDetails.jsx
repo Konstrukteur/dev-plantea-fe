be71abf19f5057533427fd1d7f3d46e6ff83056b
alfarrobeira <122460653+alfarrobeira@users.noreply.github.com>
2023-05-02 14:47:22 +0200

    centralize API calls in utils file

--- src/components/RecipeDetails.jsx
+++ src/components/RecipeDetails.jsx
@@ -1,47 +1,36 @@
 import { useState, useEffect } from "react";
 import { useOutletContext, useParams } from "react-router-dom";
 import RecipeItem from "./RecipeItem";
-
+import utils from "../services/utils.jsx";
 
 const RecipeDetails = () => {
-    const { pageTitle, setPageTitle } = useOutletContext();
-    const title = useParams();
-    const [recipe, setRecipe] = useState();
-    // const secretFragment = `?key=${process.env.REACT_APP_PERENIAL_KEY}`;
-    const baseUrl = "https://plantea.aladlabs.net/api/v1/recipes/";
-    // const baseUrl = "http://10.0.1.22:8000/api/v1/recipes/";
-    const initialUrl = baseUrl + title.id;
-    const [apiUrl, setApiUrl] = useState(initialUrl);
+  const { pageTitle, setPageTitle } = useOutletContext();
+  const { id } = useParams();
+  const [recipe, setRecipe] = useState(); 
+  const { getSingleRecipe } = utils();
 
-    useEffect(() => {
-        // todo: update with title of recipe
-        setPageTitle("Recipe Details");
-        getData();
-    }, [apiUrl]);
+  useEffect(() => {  
+    getSingleRecipe(id).then(recipe => 
+      {   
+        setRecipe(recipe);
+        setPageTitle(recipe.name);
+      });
 
-    const getData = async () => {
-        try {
-            const response = await fetch(apiUrl);
-            const json = await response.json();
-            setRecipe(json);
-        } catch (error) {
-            console.log(error.message);
-        }
-    };
+  }, [id]);
 
-    return (
-        <div className=''>
-            <div>
-                {recipe ? (
-                    <div>
-                        <RecipeItem key={recipe.id} id={recipe.id} name={recipe.name} image={recipe.image} portions={recipe.portions} prep_time={recipe.prep_time} cook_time={recipe.cook_time} instructions={recipe.instructions} />
-                    </div>
-                ) : (
-                    "loading"
-                )}
-            </div>
-        </div>
-    );
+  return (
+    <div className=''>
+      <div>
+        {recipe ? (
+          <div>
+            <RecipeItem key={recipe.id} id={recipe.id} name={recipe.name} image={recipe.image} portions={recipe.portions} prep_time={recipe.prep_time} cook_time={recipe.cook_time} instructions={recipe.instructions} />
+          </div>
+        ) : (
+          "loading"
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default RecipeDetails;
