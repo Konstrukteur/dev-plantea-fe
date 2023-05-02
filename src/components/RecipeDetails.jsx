16bc05bd8af99764ae831cc05f5a8db66ec8b20a
Aleksandra <shkolovaya.aleks@gmail.com>
2023-05-02 21:00:02 +0200

    Recipe details has listed ingredients and instructions

--- src/components/RecipeDetails.jsx
+++ src/components/RecipeDetails.jsx
@@ -4,33 +4,48 @@ import RecipeItem from "./RecipeItem";
 import utils from "../services/utils.jsx";
 
 const RecipeDetails = () => {
-  const { pageTitle, setPageTitle } = useOutletContext();
-  const { id } = useParams();
-  const [recipe, setRecipe] = useState(); 
-  const { getSingleRecipe } = utils();
+    const { pageTitle, setPageTitle } = useOutletContext();
+    const { id } = useParams();
+    const [recipe, setRecipe] = useState();
+    const { getSingleRecipe } = utils();
 
-  useEffect(() => {  
-    getSingleRecipe(id).then(recipe => 
-      {   
-        setRecipe(recipe);
-        setPageTitle(recipe.name);
-      });
+    const makeIngredientsArray = (recipeData) => {
+        let array = [];
 
-  }, [id]);
 
-  return (
-    <div className=''>
-      <div>
-        {recipe ? (
-          <div>
-            <RecipeItem key={recipe.id} id={recipe.id} name={recipe.name} image={recipe.image} portions={recipe.portions} prep_time={recipe.prep_time} cook_time={recipe.cook_time} instructions={recipe.instructions} />
-          </div>
-        ) : (
-          "loading"
-        )}
-      </div>
-    </div>
-  );
+        for (const [key, value] of Object.entries(recipeData)) {
+            if (key.includes('strIngredient') && value !== '') {
+                array.push(value)
+            }
+
+        }
+        return array
+    }
+
+    useEffect(() => {
+        getSingleRecipe(id).then(recipe => {
+            recipe.ingredientsArray = makeIngredientsArray(recipe);
+
+            setRecipe(recipe);
+            setPageTitle(recipe.strMeal);
+
+        });
+
+    }, [id]);
+
+    return (
+        <div className=''>
+            <div>
+                {recipe ? (
+                    <div>
+                        <RecipeItem ingredientsArray={recipe.ingredientsArray} key={recipe.idMeal} idMeal={recipe.idMeal} strMealThumb={recipe.strMealThumb} strInstructions={recipe.strInstructions} />
+                    </div>
+                ) : (
+                    "loading"
+                )}
+            </div>
+        </div>
+    );
 };
 
 export default RecipeDetails;
